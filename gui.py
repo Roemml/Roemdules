e95472bf1265dc45e6d20cc49565c8a7e6b96451
Dominik Wagner <Roemml@gmx.de>
2025-06-22 18:21:49 +0200

    erstelle_Fenster nutzbar

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 #Python Imports
 import tkinter as tk
 #Globalle Konstanzen
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 ALIGN_LEFT = "0"
 ALIGN_CENTER = "1"
 def parse_geometry(geometry:str) -> tuple[int,int]:
@@ -10,7 +10,7 @@ def parse_geometry(geometry:str) -> tuple[int,int]:
     """
     split=geometry.split("x")
     return (int(split[0]), split[1].split("+")[0])
-def erstelle_Fenster(*widgets:dict, fenster_name:str = "Fenster", fenster_breite:int = 0, fenster_hoehe:int = 0, protocols:tuple[tuple[str, str]] = None) -> tk.Tk:
+def erstelle_Fenster(*widgets:dict, fenster_name:str = "Fenster", fenster_breite:int = 0, fenster_hoehe:int = 0, protocols:tuple[tuple[str, str]] = None, context:dict = None) -> tk.Tk:
     """
     Erstellt dynamisch ein Fenster in der Mitte des aktuellen Bildschirms.
 
@@ -23,6 +23,7 @@ def erstelle_Fenster(*widgets:dict, fenster_name:str = "Fenster", fenster_breite
         fenster_hoehe - Integer: Höhe des Fensters in Pixel, wenn nicht mitgegeben oder 0, oder nicht alles drauf passt, wird die Größe berechnet.
         protocol - Tupel(String,String) Name des protokolls und Name der Funktion. die Funktion wird automatisch mit Lambda aufgerufen für Parameter
         widget - Dictionary - Für jedes Element auf dem Fenster von Oben bis unten ein Dictionary, dass das Element beschreibt
+        context - Dictionary für Übergabe von benötigten Variablen oder Funktionen, die nicht im Modul sind.
     """
     fenster = tk.Tk()
     fenster.title = fenster_name
@@ -32,20 +33,28 @@ def erstelle_Fenster(*widgets:dict, fenster_name:str = "Fenster", fenster_breite
     if not protocols == None:
         for protocol in protocols:
             try:
-                if protocol[1].find("(") != -1:
-                    procdict = {}
-                    einzel_argument = ""
-                    protocol_funktion = protocol[1]
-                    protocol_funktion = protocol_funktion.lstrip()
-                    for char in protocol_funktion:
-                        if str(char).isalnum() or str(char) == "_":
-                            einzel_argument += str(char)
-                        elif einzel_argument != "":
+                # funktionsname = protocol[2].__name__
+                # if funktionsname not in globals():
+                #     globals()[funktionsname] = protocol[2]
+                procdict = {}
+                einzel_argument = ""
+                protocol_funktion = protocol[1].lstrip()
+                for char in protocol_funktion:
+                    if str(char).isalnum() or str(char) == "_":
+                        einzel_argument += str(char)
+                    elif einzel_argument != "":
+                        if context and einzel_argument in context:
+                            procdict[einzel_argument] = context[einzel_argument]
+                        else:
                             procdict[einzel_argument] = eval(einzel_argument)
-                            einzel_argument=""
-                    fenster.protocol(protocol[0], lambda pc=protocol,pd=procdict: eval(pc[1], pd))    
-                else:
-                    fenster.protocol(protocol[0], lambda pc=protocol: eval(pc[1]))
+                        einzel_argument=""
+                if not einzel_argument == "":
+                    if context and einzel_argument in context:
+                        procdict[einzel_argument] = context[einzel_argument]
+                    else:
+                        procdict[einzel_argument] = eval(einzel_argument)
+                    einzel_argument=""
+                fenster.protocol(protocol[0], lambda pc=protocol,pd=procdict: eval(pc[1], pd))    
             except Exception as e:
                 print(f"Protokoll {protocol} für Fenster {fenster_name} konnte nicht erstellt werden: {e}")
     for widget in widgets:
@@ -66,20 +75,29 @@ def erstelle_Fenster(*widgets:dict, fenster_name:str = "Fenster", fenster_breite
                 print(f"Fehler beim Label erstellen: {e}")
         elif widget["type"] == "button":
             try:
-                if widget["command"].find("(") != -1:
-                    comdict = {}
-                    einzel_argument = ""
-                    command_funktion = widget["command"]
-                    command_funktion = command_funktion.lstrip()
-                    for char in command_funktion:
-                        if str(char).isalnum() or str(char) == "_":
-                            einzel_argument += str(char)
-                        elif einzel_argument  != "":
+                # funktionsname = widget["function"].__name__
+                # if funktionsname not in globals():
+                #     globals()[funktionsname] = widget["function"]
+                comdict = {}
+                einzel_argument = ""
+                command_funktion_string = widget["command"]
+                command_funktion_string = command_funktion_string.lstrip()
+                for char in command_funktion_string:
+                    if str(char).isalnum() or str(char) == "_":
+                        einzel_argument += str(char)
+                    elif einzel_argument  != "":
+                        if context and einzel_argument in context:
+                            comdict[einzel_argument] = context[einzel_argument]
+                        else:
                             comdict[einzel_argument] = eval(einzel_argument)
-                            einzel_argument = ""
-                    element = tk.Button(fenster, text = widget["text"], command = lambda wc = widget["command"], cd = comdict: eval(wc, cd), width = widget["width"], height = widget["height"])
-                else:
-                    element = tk.Button(fenster, text = widget["text"], command = lambda wc = widget["command"]: eval(wc), width=widget["width"], height = widget["height"])
+                        einzel_argument = ""
+                if not einzel_argument == "":
+                    if context and einzel_argument in context:
+                        comdict[einzel_argument] = context[einzel_argument]
+                    else:
+                        comdict[einzel_argument] = eval(einzel_argument)
+                einzel_argument=""
+                element = tk.Button(fenster, text = widget["text"], command = lambda wc = widget["command"], cd = comdict: eval(wc, cd), width = widget["width"], height = widget["height"])
                 element_top = min_hoehe
                 min_hoehe += element.winfo_reqheight()
                 if element.winfo_reqwidth() > min_breite: min_breite = element.winfo_reqwidth()
