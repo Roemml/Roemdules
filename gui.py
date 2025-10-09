225ca9b94748e0ecd03778b5121d983d6c595d26
Dominik Wagner <Roemm@gmx.de>
2025-10-09 21:55:49 +0200

    update

--- gui.py
+++ gui.py
@@ -10,7 +10,7 @@ def parse_geometry(geometry:str) -> tuple[int,int]:
     """
     split=geometry.split("x")
     return (int(split[0]), split[1].split("+")[0])
-def erstelle_Fenster(widgets:list[dict], fenster_name:str = "Fenster", fenster_breite:int = 0, fenster_hoehe:int = 0, protocols:tuple[tuple[str, str]] = None, context:dict = None) -> tk.Tk:
+def erstelle_Fenster(widgets:list[dict], fenster_name:str = "Fenster", fenster_breite:int = 0, fenster_hoehe:int = 0, geometry_manager:str = "place", protocols:tuple[tuple[str, str]] = None, context:dict = None) -> tk.Tk:
     """
     Erstellt dynamisch ein Fenster in der Mitte des aktuellen Bildschirms.
 
@@ -30,7 +30,7 @@ def erstelle_Fenster(widgets:list[dict], fenster_name:str = "Fenster", fenster_b
     min_breite = 0
     min_hoehe = 0
     varlist = {}
-    elemente:tuple[tuple[tk.Widget,int]] = []
+    elemente:list[tk.Widget] = []
     if not protocols == None:
         for protocol in protocols:
             try:
@@ -66,12 +66,14 @@ def erstelle_Fenster(widgets:list[dict], fenster_name:str = "Fenster", fenster_b
         elif widget["type"] == "label": 
             try:
                 element = tk.Label(fenster, text = widget["text"])
-                element_top=min_hoehe
+                element.element_top=min_hoehe
                 min_hoehe+=element.winfo_reqheight()
                 if element.winfo_reqwidth()>min_breite:min_breite=element.winfo_reqwidth()
-                if "align" in widget: align = widget["align"]
-                else: align = ALIGN_CENTER
-                elemente.append((element, element_top, align))
+                if "align" in widget: element.align = widget["align"]
+                else: element.align = ALIGN_CENTER
+                if "name" in widget:
+                    element.name = widget["name"]
+                elemente.append(element)
             except Exception as e:
                 print(f"Fehler beim Label erstellen: {e}")
         elif widget["type"] == "button":
@@ -99,12 +101,13 @@ def erstelle_Fenster(widgets:list[dict], fenster_name:str = "Fenster", fenster_b
                         comdict[einzel_argument] = eval(einzel_argument)
                 einzel_argument=""
                 element = tk.Button(fenster, text = widget["text"], command = lambda wc = widget["command"], cd = comdict: eval(wc, cd), width = widget["width"], height = widget["height"])
-                element_top = min_hoehe
+                element.element_top = min_hoehe
                 min_hoehe += element.winfo_reqheight()
                 if element.winfo_reqwidth() > min_breite: min_breite = element.winfo_reqwidth()
                 if element.winfo_reqwidth() > min_breite: min_breite = element.winfo_reqwidth()
-                if "align" in widget: align = widget["align"]
-                elemente.append((element, element_top, align))
+                if "align" in widget: element.align = widget["align"]
+                else: element.align = ALIGN_CENTER
+                elemente.append(element)
             except Exception as e:
                 print(f"Fehler beim Button erstellen: {e}")
         elif widget["type"] == "entry": 
@@ -114,12 +117,12 @@ def erstelle_Fenster(widgets:list[dict], fenster_name:str = "Fenster", fenster_b
                     element.config({"show": widget["show"]})
                 if "name" in widget:
                     element.name = widget["name"]
-                element_top=min_hoehe
+                element.element_top=min_hoehe
                 min_hoehe+=element.winfo_reqheight()
                 if element.winfo_reqwidth()>min_breite:min_breite=element.winfo_reqwidth()
-                if "align" in widget: align = widget["align"]
-                else: align = ALIGN_CENTER
-                elemente.append((element, element_top, align))
+                if "align" in widget: element.align = widget["align"]
+                else: element.align = ALIGN_CENTER
+                elemente.append(element)
             except Exception as e:
                 print(f"Fehler beim Eingabefeld erstellen: {e}")
         elif widget["type"] == "radiobutton":
@@ -130,24 +133,52 @@ def erstelle_Fenster(widgets:list[dict], fenster_name:str = "Fenster", fenster_b
                   # Setze den Startwert von modus aus context, falls vorhanden
                 if 'modus' in context:
                     varlist[widget["variable"]].set(context['modus'])  # Dynamischer Startwert aus context
-                element = tk.Radiobutton(fenster, variable=varlist[widget["variable"]], value=widget["value"], text=widget["text"])
-                element_top=min_hoehe
+                if 'command' in widget:
+                    comdict = {}
+                    einzel_argument = ""
+                    command_funktion_string = widget["command"]
+                    command_funktion_string = command_funktion_string.lstrip()
+                    for char in command_funktion_string:
+                        if str(char).isalnum() or str(char) == "_":
+                            einzel_argument += str(char)
+                        elif einzel_argument  != "":
+                            if context and einzel_argument in context:
+                                comdict[einzel_argument] = context[einzel_argument]
+                            else:
+                                comdict[einzel_argument] = eval(einzel_argument)
+                            einzel_argument = ""
+                    if not einzel_argument == "":
+                        if context and einzel_argument in context:
+                            comdict[einzel_argument] = context[einzel_argument]
+                        else:
+                            comdict[einzel_argument] = eval(einzel_argument)
+                    einzel_argument=""
+                    element = tk.Radiobutton(fenster, variable=varlist[widget["variable"]], value=widget["value"], text=widget["text"], command = lambda wc = widget["command"], cd = comdict: eval(wc, cd))
+                else:
+                    element = tk.Radiobutton(fenster, variable=varlist[widget["variable"]], value=widget["value"], text=widget["text"])
+                element.element_top=min_hoehe
                 min_hoehe+=element.winfo_reqheight()
                 if element.winfo_reqwidth()>min_breite:min_breite=element.winfo_reqwidth()
-                if "align" in widget: align = widget["align"]
-                else: align = ALIGN_CENTER
-                elemente.append((element, element_top, align))
+                if "align" in widget: element.align = widget["align"]
+                else: element.align = ALIGN_CENTER
+                elemente.append(element)
             except Exception as e:
                 print(f"Fehler beim radiobutton erstellen: {e}")
         else:
             print(f"{widget["type"]} ist kein gültuger Widget typ")
+    min_breite += 10
+    min_hoehe += 10
     if min_breite > fenster_breite:
         fenster_breite = min_breite
     if min_hoehe > fenster_hoehe:
         fenster_hoehe = min_hoehe
     for element in elemente:
-        if element[2] == ALIGN_CENTER: element[0].place(x = (fenster_breite - element[0].winfo_reqwidth()) // 2, y = element[1])
-        elif element[2] == ALIGN_LEFT: element[0].place(x = 0, y = element[1])
+        if geometry_manager == "place":
+            if element.align == ALIGN_CENTER: element.place(x = (fenster_breite - element.winfo_reqwidth()) // 2, y = element.element_top)
+            elif element.align == ALIGN_LEFT: element.place(x = 0, y = element.element_top)
+        elif geometry_manager == "pack":
+            if element.align == ALIGN_CENTER: element.pack(anchor=tk.CENTER)
+            elif element.align == ALIGN_LEFT: element.pack(anchor=tk.W)
     x_pos = (fenster.winfo_screenwidth() - fenster_breite) // 2
     if x_pos < 0: x_pos = 0
     y_pos = (fenster.winfo_screenheight() - fenster_hoehe) // 2
